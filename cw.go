097a48276c5b45baa316918630793b2e1732a081
Simon Zimmermann <simonz05@gmail.com>
2012-03-20 15:46:54 +0100

    simplify

--- cw.go
+++ cw.go
@@ -16,14 +16,78 @@ type fetchState struct {
     state bool
 }
 
+type urlResult struct {
+    id   string
+    data []byte
+    url  *url.URL
+}
+
 var (
     logger = log.New(os.Stdout, "", 0)
 )
 
-func usage() {
-    fmt.Fprintf(os.Stdout, "usage: cw url [url ...]\n")
-    flag.PrintDefaults()
-    os.Exit(0)
+func (u *urlResult) storageWriter(status chan<- fetchState) {
+    //logger.Printf("%s\n", u.data)
+    status <- fetchState{"", true}
+}
+
+func (u *urlResult) getUrl(status, result chan<- fetchState) {
+    urlstr := u.url.String()
+    sep := strings.Index(urlstr, "?")
+    fs := fetchState{urlstr, true}
+
+    if sep > 0 {
+        urlstr = urlstr[:sep]
+    }
+
+    res, err := http.Get(urlstr)
+
+    if err != nil {
+        fs.state = false
+        status <- fs
+        return
+    }
+
+    debugResponse(res)
+
+    u.data, err = ioutil.ReadAll(res.Body)
+
+    if err != nil {
+        fs.state = false
+        status <- fs
+        return
+    }
+
+    res.Body.Close() /* close fd */
+    go u.storageWriter(result)
+    status <- fs
+}
+
+func getAndListenUrls(urls []*url.URL) {
+    netlen := len(urls)
+    fslen := netlen
+    netState := make(chan fetchState)
+    fsState := make(chan fetchState)
+
+    for _, u := range urls {
+        ur := &urlResult{url: u}
+        go ur.getUrl(netState, fsState)
+    }
+
+    for {
+        select {
+        case fs := <-netState:
+            debugFetchState(&fs)
+            netlen--
+        case fs := <-fsState:
+            debugFetchState(&fs)
+            fslen--
+        }
+
+        if netlen == 0 && fslen == 0 {
+            break
+        }
+    }
 }
 
 func debugFetchState(fs *fetchState) {
@@ -56,78 +120,10 @@ func debugResponse(r *http.Response) {
     }
 }
 
-func storageWriter(data []byte) {
-    //logger.Printf("%s\n", data)
-}
-
-func storageListener(status chan<- fetchState, in <-chan []byte) {
-    for {
-        data := <-in
-        storageWriter(data)
-        fs := fetchState{"", true}
-        status <- fs
-    }
-}
-
-func getUrl(status chan<- fetchState, result chan<- []byte, u *url.URL) {
-    urlstr := u.String()
-    sep := strings.Index(urlstr, "?")
-    fs := fetchState{urlstr, true}
-
-    if sep > 0 {
-        urlstr = urlstr[:sep]
-    }
-
-    res, err := http.Get(urlstr)
-
-    if err != nil {
-        fs.state = false
-        status <- fs
-        return
-    }
-
-    debugResponse(res)
-
-    data, err := ioutil.ReadAll(res.Body)
-
-    if err != nil {
-        fs.state = false
-        status <- fs
-        return
-    }
-
-    res.Body.Close() /* close fd */
-    result <- data
-    status <- fs
-}
-
-func getAndListenUrls(urls []*url.URL) {
-    netlen := len(urls)
-    fslen := netlen
-    netState := make(chan fetchState)
-    fsState := make(chan fetchState)
-    results := make(chan []byte, 100)
-
-    go storageListener(fsState, results)
-
-    for _, u := range urls {
-        go getUrl(netState, results, u)
-    }
-
-    for {
-        select {
-        case fs := <-netState:
-            debugFetchState(&fs)
-            netlen--
-        case fs := <-fsState:
-            debugFetchState(&fs)
-            fslen--
-        }
-
-        if netlen == 0 && fslen == 0 {
-            break
-        }
-    }
+func usage() {
+    fmt.Fprintf(os.Stdout, "usage: cw url [url ...]\n")
+    flag.PrintDefaults()
+    os.Exit(0)
 }
 
 func main() {
